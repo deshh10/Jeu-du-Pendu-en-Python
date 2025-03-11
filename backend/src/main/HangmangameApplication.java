fd6cf2780330e57bec9aad6f41923964b4df992b
deshh10 <deshani.kumarathas@viacesi.fr>
2025-03-11 10:12:03 +0100

    Create HangmangameApplication.java

---
+++ backend/src/main/HangmangameApplication.java
@@ -0,0 +1,13 @@
+package me.topeestla.hangmangame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HangmangameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HangmangameApplication.class, args);
+	}
+
+}
